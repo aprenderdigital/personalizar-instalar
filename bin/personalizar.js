b0df02f1e5e71f1e9c47a96be7e1d0f9673a8851
aprenderdigital <38329393+aprenderdigital@users.noreply.github.com>
2019-05-13 19:02:30 -0300

    Update personalizar.js

--- bin/personalizar.js
+++ bin/personalizar.js
@@ -3,6 +3,8 @@
 var shell = require("shelljs");
 shell.exec("echo shell.exec works");
 
+//https://github.com/aprenderdigital/npm-petermbenjamin
+
 //npm version major | minor | patch
 //npm publish
 //git clone
